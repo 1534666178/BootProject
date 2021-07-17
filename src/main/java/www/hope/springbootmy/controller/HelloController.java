8ee1336279672a93fc2df7f2f9c9b103a0c98abc
CoderG <1534666178@qq.com>
2021-07-17 10:56:27 +0800

    修改HelloController

--- src/main/java/www/hope/springbootmy/controller/HelloController.java
+++ src/main/java/www/hope/springbootmy/controller/HelloController.java
@@ -1,11 +1,9 @@
 package www.hope.springbootmy.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import www.hope.springbootmy.domain.Dog;
 import www.hope.springbootmy.domain.Person;
 
 import java.util.HashMap;
